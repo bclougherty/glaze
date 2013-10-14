cbaaf2db485a0d3f32d5b894384ad26ae99a80a6
Brendan Clougherty <brendan.clougherty@logicnation.com>
2013-10-13 23:09:01 -0400

    Initial import

---
+++ controllers/public.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/octoberxp/glaze/core"
+)
+
+type Public struct {
+	*core.Controller
+}
+
+func NewPublicController() *Public {
+	controller, err := core.NewController()
+	if err != nil {
+		panic(err)
+	}
+
+	controller.LoadTemplates(core.TemplatePath + "public/*.html")
+
+	return &Public{Controller: controller}
+}
+
+func (controller *Public) Index(w http.ResponseWriter, r *http.Request) {
+	controller.RenderTemplate(w, "index", nil)
+}
---
+++ core/core.go
@@ -0,0 +1,62 @@
+package core
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type TemplateMap map[string]*template.Template
+
+type Controller struct {
+	BaseTemplate *template.Template
+	Templates    TemplateMap
+}
+
+var TemplatePath = os.ExpandEnv("$GOPATH/src/skepticalpengu.in/penguinbook/views/")
+
+func NewController() (*Controller, error) {
+	tmpl, err := template.ParseFiles(TemplatePath + "main/base.html")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Controller{BaseTemplate: tmpl}, nil
+}
+
+func (controller *Controller) RenderPage(writer http.ResponseWriter, templateName string, data interface{}) {
+	err := controller.Templates[templateName+".html"].Execute(writer, data)
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (controller *Controller) RenderTemplate(writer http.ResponseWriter, templateName string, data interface{}) {
+	err := controller.Templates[templateName+".html"].Execute(writer, data)
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (controller *Controller) LoadTemplates(templatePath string) {
+	files, err := filepath.Glob(templatePath)
+	if err != nil {
+		panic(err)
+	}
+
+	templates := make(map[string]*template.Template, len(files))
+	for _, file := range files {
+		thisTemplate, err := controller.BaseTemplate.Clone()
+		thisTemplate, err = thisTemplate.ParseFiles(file)
+		if err != nil {
+			panic(err)
+		}
+
+		templates[filepath.Base(file)] = thisTemplate
+	}
+
+	controller.Templates = templates
+}

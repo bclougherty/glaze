0212da6540dca5760a6619d3673ac6fbf3b849cd
Brendan Clougherty <bclougherty@sfscapital.com>
2014-08-26 14:42:09 -0400

    Cleanup

--- glaze/controller.go
+++ glaze/controller.go
@@ -1,8 +1,10 @@
 package glaze
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"path"
 	"path/filepath"
 )
 
@@ -17,7 +19,7 @@ type Controller struct {
 type TemplateMap map[string]*template.Template
 
 func NewController(templatePath string) (*Controller, error) {
-	tmpl, err := template.ParseFiles(templatePath + "main/base.html")
+	tmpl, err := template.ParseFiles(path.Join(templatePath, "layouts/default.html"))
 	if err != nil {
 		return nil, err
 	}
@@ -42,10 +44,16 @@ func (controller *Controller) RenderTemplate(writer http.ResponseWriter, templat
 }
 
 func (controller *Controller) LoadTemplates(templatePath string) {
-	files, err := filepath.Glob(controller.templatePath + templatePath)
+	fullpath := path.Join(controller.templatePath, templatePath, "*.html")
+
+	fmt.Printf("Loading templates from \"%s\"... ", fullpath)
+
+	files, err := filepath.Glob(fullpath)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("Found %d\n", len(files))
 
 	templates := make(map[string]*template.Template, len(files))
 	for _, file := range files {
